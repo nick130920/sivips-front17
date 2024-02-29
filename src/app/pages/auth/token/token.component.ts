d7d9678c29e8c3ac4fc242531151819133942b32
julian-trujillo1 <julian.trujillo@usco.edu.co>
2024-02-29 16:11:15 -0500

    navbar and guard

---
+++ src/app/pages/auth/token/token.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-token',
+    standalone: true,
+    imports: [],
+    templateUrl: './token.component.html',
+})
+export class TokenComponent {}
