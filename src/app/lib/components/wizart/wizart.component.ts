321990b2f9b6b1b7025351c89e016cef7f8dc408
Coaxus <trujillojulian.024@gmail.com>
2024-03-04 18:09:41 -0500

    wizart init

---
+++ src/app/lib/components/wizart/wizart.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-wizart',
+    standalone: true,
+    imports: [],
+    templateUrl: './wizart.component.html',
+})
+export class WizartComponent {}
