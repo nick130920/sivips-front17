93af3a9ca73219b11d77d16b00787025fdcd1432
Nicolas Muñoz <nicolas.munoz@usco.edu.co>
2024-03-05 15:26:53 -0500

    login token response

--- src/app/lib/services/auth/auth.service.ts
+++ src/app/lib/services/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { storage } from '@lib/utils/storage/storage.utils';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, tap } from 'rxjs';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { environment } from '@env/environment';
 @Injectable({
@@ -23,10 +23,28 @@ export class AuthService {
         this._http
             .post(url, body.toString(), {
                 headers,
+                observe: 'response',
             })
-            .subscribe((response) => {
-                console.log(response);
-            });
+            .pipe(
+                tap((response) => {
+                    console.log(response);
+                    // Extrae los encabezados de la respuesta
+                    const responseHeaders = response.headers;
+                    console.log(responseHeaders);
+
+                    // Extrae el token del encabezado deseado (en este caso, 'authorization')
+                    const token = responseHeaders.get('Authorization');
+
+                    if (token) {
+                        console.log('Token extraído:', token);
+                        // Utiliza el token para acciones adicionales (almacenamiento seguro, enviar a otra solicitud, etc.)
+                    } else {
+                        console.error('Token no encontrado en los encabezados de la respuesta');
+                        // Maneja el caso en el que el token esté ausente
+                    }
+                }),
+            )
+            .subscribe();
         storage.setItem('appSession', { user: 'some-user-id', token: 'abc' });
         this.isAuthenticated$.next(true);
     }
--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -17,10 +17,8 @@ export class LoginComponent implements OnInit {
     @Input() returnUrl!: string;
     faculty!: Faculty;
     loginForm = new FormGroup({
-        // eslint-disable-next-line @typescript-eslint/unbound-method
-        username: new FormControl('', [Validators.required]),
-        // eslint-disable-next-line @typescript-eslint/unbound-method
-        password: new FormControl('', [Validators.required]),
+        username: new FormControl('', [Validators.required.bind(Validators)]),
+        password: new FormControl('', [Validators.required.bind(Validators)]),
     });
     constructor(
         private readonly _router: Router,
@@ -30,8 +28,8 @@ export class LoginComponent implements OnInit {
     firstStepLogin(): void {
         if (this.loginForm.valid) {
             this._authService.login(this.loginForm.value.username as string, this.loginForm.value.password as string);
+            this._router.navigate(['/home']);
         }
-        this._router.navigate(['/home']);
     }
     secondStepLogin(): void {
         // this._authService.login();
